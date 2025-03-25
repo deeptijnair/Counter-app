ac76205d49d8a552b1e91826812e80e984b96cb3
Deepthi <11665833+deeptijnair@users.noreply.github.com>
2025-03-25 15:43:24 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a counter app following the JavaScript course on Scrimba, with added personal touches.
---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+let resetEl = document.getElementById("save-el").innerText;
+let count = 0;
+
+function increment() {
+    count++;
+    countEl.innerText = count;
+}
+
+
+function save() {
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+    count = 0;
+    countEl.innerText = count;
+}
+
+function reset(){
+    let saveEl = document.getElementById("save-el");
+    count = 0;
+    countStr = 0;
+    countEl.innerText = count;
+    saveEl.innerText = resetEl;
+}
